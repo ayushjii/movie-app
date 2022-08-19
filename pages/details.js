72cd996d0278f7dc170bdd4b1f248fe7c32267d2
ayushjii <pawangoyal6789@gmail.com>
2022-08-19 09:45:10 +0530

    t

---
+++ pages/details.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const details = () => {
+  return (
+    <div>
+      <h1 className="text-red-400 text-center font-serif font-extrabold text-6xl">Details</h1>
+    </div>
+  );
+};
+
+export default details;
